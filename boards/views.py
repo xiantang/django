e3ae3c491e138ac76f274144afa33797d167ed83
zhujingdi <34479567+xiantang@users.noreply.github.com>
2018-03-16 13:49:47 +0800

    Initial commit

---
+++ boards/views.py
@@ -0,0 +1,100 @@
+from django.db.models import Count
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import View,UpdateView
+from .forms import NewTopicForm, PostForm
+from .models import Board, Post, Topic
+from django.utils import timezone
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
+
+
+@login_required
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = request.user
+            topic.save()
+            Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=request.user
+            )
+            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    topic.views += 1
+    topic.save()
+    return render(request, 'topic_posts.html', {'topic': topic})
+
+
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+    else:
+        form = PostForm()
+    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+class NewPostView(View):
+    def post(self,request):
+        self.form=PostForm(request.POST)
+        if self.form.is_valid():
+            self.form.save()
+            return  redirect('post_list')
+        return self.render(request)
+    def get(self,request):
+        self.form=PostForm()
+        return self.render(request)
+
+    def render(self,request):
+        return render(request,'new_topic.html',{'form':self.form})
+
+
+def new_post(request):
+
+    if request.method=='POST':
+        form=PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form=PostForm
+    return render(request,'new_topic.html',{'form':form})
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message',)
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+    def form_valid(self, form):
+        post=form.save(commit=False)
+        post.updated_by=self.request.user
+        post.updated_at=timezone.now()
+        post.save()
+        return  redirect('topic_posts',pk=post.topic.board.pk)
