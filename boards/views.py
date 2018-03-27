469d12715ff919098e803586458b66d2ed3c8b26
zhujingdi <34479567+xiantang@users.noreply.github.com>
2018-03-27 18:30:38 +0800

    aaa
    aaaa

--- boards/views.py
+++ boards/views.py
@@ -1,19 +1,39 @@
 from django.db.models import Count
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import View,UpdateView
+from django.views.generic import View,UpdateView,ListView
 from .forms import NewTopicForm, PostForm
 from .models import Board, Post, Topic
 from django.utils import timezone
-
-def home(request):
-    boards = Board.objects.all()
-    return render(request, 'home.html', {'boards': boards})
+from django.utils.decorators import method_decorator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
+#
+# def home(request):
+#     boards = Board.objects.all()
+#     return render(request, 'home.html', {'boards': boards})
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'home.html'
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    queryset = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(queryset, 20)
+
+    try:
+        topics = paginator.page(page)
+    except PageNotAnInteger:
+        # fallback to the first page
+        topics = paginator.page(1)
+    except EmptyPage:
+        # probably the user tried to add a page number
+        # in the url, so we fallback to the last page
+        topics = paginator.page(paginator.num_pages)
+
     return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 
@@ -86,15 +106,17 @@ def new_post(request):
         form=PostForm
     return render(request,'new_topic.html',{'form':form})
 
+@method_decorator(login_required,name='dispatch')
 class PostUpdateView(UpdateView):
     model = Post
     fields = ('message',)
     template_name = 'edit_post.html'
     pk_url_kwarg = 'post_pk'
     context_object_name = 'post'
+
     def form_valid(self, form):
         post=form.save(commit=False)
         post.updated_by=self.request.user
         post.updated_at=timezone.now()
         post.save()
-        return  redirect('topic_posts',pk=post.topic.board.pk)
+        return  redirect('topic_posts',pk=post.topic.board.pk, topic_pk=post.topic.pk)
